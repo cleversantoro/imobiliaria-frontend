f080a12b3abdebb571229683e5cd5f8f9d57a205
Clever Santoro <cleversantoro@gmail.com>
2025-11-05 18:21:52 -0300

    ajuste upload

--- src/app/core/services/property-api.service.ts
+++ src/app/core/services/property-api.service.ts
@@ -1,6 +1,6 @@
 import { inject, Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Observable, of } from 'rxjs';
+import { forkJoin, Observable, of } from 'rxjs';
 import { catchError, map, switchMap } from 'rxjs/operators';
 
 import { environment } from '../../../environments/environment';
@@ -82,9 +82,44 @@ export class PropertyApiService {
   getProperties(query: PropertyQuery = {}): Observable<Property[]> {
     const params = buildHttpParams(this.mapQueryToParams(query));
 
-    return this.http
-      .get<ImovelResponse[]>(this.collectionUrl, { params })
-      .pipe(map((imoveis) => imoveis.map((imovel) => this.mapImovelToProperty(imovel))));
+    return this.http.get<ImovelResponse[]>(this.collectionUrl, { params }).pipe(
+      switchMap((imoveis) => {
+        if (!imoveis.length) {
+          return of([]);
+        }
+
+        const baseProperties = imoveis.map((imovel) => this.mapImovelToProperty(imovel));
+
+        const limit = typeof query.limit === 'number' && query.limit > 0 ? query.limit : baseProperties.length;
+        const propertiesToEnhance = baseProperties.slice(0, limit);
+
+        if (!propertiesToEnhance.length) {
+          return of(baseProperties);
+        }
+
+        const enhancements$ = propertiesToEnhance.map((property) =>
+          this.fetchPropertyPhotos(String(property.id)).pipe(
+            catchError(() => of([])),
+            map((photos) => (photos.length ? { ...property, images: photos } : property)),
+          ),
+        );
+
+        return forkJoin(enhancements$).pipe(
+          map((enhancedProperties) => {
+            const merged = [...baseProperties];
+
+            enhancedProperties.forEach((enhanced) => {
+              const targetIndex = merged.findIndex((item) => item.id === enhanced.id);
+              if (targetIndex !== -1) {
+                merged[targetIndex] = enhanced;
+              }
+            });
+
+            return merged;
+          }),
+        );
+      }),
+    );
   }
 
   getPropertyById(id: string | number): Observable<Property> {
